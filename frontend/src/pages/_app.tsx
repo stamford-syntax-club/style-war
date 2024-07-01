8c1f73726a59c8729800a3469c53a5e01c60605a
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-07-01 22:11:34 +0700

    frontend template setup

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,41 @@
+import "@mantine/core/styles.css";
+import "../styles/globals.css";
+
+import type { AppProps } from "next/app";
+import { createTheme, MantineProvider } from "@mantine/core";
+import Head from "next/head";
+
+const theme = createTheme({
+  colors: {
+    dark: [
+      "#C1C2C5",
+      "#A6A7AB",
+      "#909296",
+      "#5c5f66",
+      "#373A40",
+      "#2C2E33",
+      "#25262b",
+      "#1A1B1E",
+      "#141517",
+      "#101113",
+    ],
+  },
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Style War</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider theme={theme} defaultColorScheme="dark">
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+}
