0b51ac7ecbf8db0e45035f0e2b46d767f210fb33
chinathaip <cartoonabe@gmail.com>
2024-07-04 03:56:14 +0700

    frontend: init admin page with auth redirect

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,23 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/playground"]);
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+
+export default clerkMiddleware(
+  (auth, req) => {
+    if (isAdminRoute(req)) {
+      auth().protect((has) => {
+        return has({ role: "org:admin" });
+      });
+    }
+
+    if (isProtectedRoute(req)) {
+      auth().protect();
+    }
+  },
+  { debug: true },
+);
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
