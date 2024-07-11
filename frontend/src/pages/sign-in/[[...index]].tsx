debe593056d25f45bc53810780400344fda47166
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-07-11 23:11:22 +0700

    center sign-in component

--- frontend/src/pages/sign-in/[[...index]].tsx
+++ frontend/src/pages/sign-in/[[...index]].tsx
@@ -1,5 +1,10 @@
 import { SignIn } from "@clerk/nextjs";
+import { Flex } from "@mantine/core";
 
 export default function SignInPage() {
-  return <SignIn />;
+  return (
+    <Flex justify="center" align="center" h="90vh">
+      <SignIn />
+    </Flex>
+  );
 }
