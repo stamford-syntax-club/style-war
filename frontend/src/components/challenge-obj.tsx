73c1691d14b8e1f6e09fcf2abf25dc5588c36ff0
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-07-11 21:45:07 +0700

    afew changes on challenge-obj

--- frontend/src/components/challenge-obj.tsx
+++ frontend/src/components/challenge-obj.tsx
@@ -2,17 +2,16 @@ import { Box, Title, Paper } from "@mantine/core";
 import React from "react";
 
 type value = { value: string };
-function Obj({ value }: value) {
+
+export default function Obj({ value }: value) {
   return (
-    <>
       <Box>
         <Title>Objectives</Title>
         <Paper className="border border-gray-600 rounded p-1 h-[80vh] w-[50vw]">
           {value}
         </Paper>
       </Box>
-    </>
   );
 }
 
-export default Obj;
+
