4f61fd48edd059d15aef8427240f82de97279056
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-07-27 23:24:55 +0700

    set notification on web socket connection

--- frontend/src/pages/playground.tsx
+++ frontend/src/pages/playground.tsx
@@ -1,9 +1,10 @@
-import { Container, Flex, Text } from "@mantine/core";
+import { Container, Flex, Notification, rem } from "@mantine/core";
 import CodeEditor from "@/components/code-editor";
 import Preview from "@/components/preview";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSocket } from "@/lib/websocket/ws";
 import { useCode } from "@/lib/data-hooks/use-code";
+import { IconCheck, IconX } from "@tabler/icons-react";
 
 interface Message {
   event: string;
@@ -12,6 +13,14 @@ interface Message {
 
 export default function Playground() {
   const { data: codeData, isLoading, isError } = useCode(1);
+  const [remainingTime, setRemainingTime] = useState<number | null>(null);
+  const [showNotification, setShowNotification] = useState<{
+    show: boolean;
+    message: string;
+    color: string;
+  }>({ show: false, message: "", color: "" });
+  const xIcon = <IconX style={{ width: rem(20), height: rem(20) }} />;
+  const checkIcon = <IconCheck style={{ width: rem(20), height: rem(20) }} />;
   const [value, setValue] = useState(
     `<!DOCTYPE html>
 <html>
@@ -20,9 +29,8 @@ export default function Playground() {
     </head>
     <body>
     </body>
-</html>`,
+</html>`
   );
-  const [remainingTime, setRemainingTime] = useState<number | null>(null);
 
   const socket = useSocket((message) => {
     try {
@@ -34,6 +42,28 @@ export default function Playground() {
     }
   });
 
+  useEffect(() => {
+    if (socket) {
+      setShowNotification({
+        show: true,
+        message: "Connected to the backend!",
+        color: "green",
+      });
+    } else if (isError || !socket) {
+      setShowNotification({
+        show: true,
+        message: "Failed to connect to the backend!",
+        color: "red",
+      });
+    }
+
+    const timer = setTimeout(() => {
+      setShowNotification((prev) => ({ ...prev, show: false }));
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [socket, isError]);
+
   const handleChangeValue = (newValue: string | undefined) => {
     if (!newValue) return;
 
@@ -44,16 +74,44 @@ export default function Playground() {
           code: newValue,
           challengeId: 1, // TODO: get from backend
         },
-      }),
+      })
     );
     setValue(newValue);
   };
 
-
   return (
     <Container fluid>
+      {/* notification box */}
+      {showNotification.show && (
+        <Notification
+          icon={showNotification.color === "green" ? checkIcon : xIcon}
+          color={showNotification.color}
+          title={showNotification.color === "green" ? "All Good!" : "Opps! Bummer"}
+          onClose={() =>
+            setShowNotification((prev) => ({
+              ...prev,
+              show: false,
+            }))
+          }
+          styles={{
+            root: {
+              position: "fixed",
+              bottom: rem(16),
+              right: rem(16),
+              zIndex: 1000,
+            },
+          }}
+        >
+          {showNotification.message}
+        </Notification>
+      )}
+
       <Flex justify="center" gap="md" align="center" mt="md">
-        <CodeEditor value={value} onChange={handleChangeValue} remainingTime={remainingTime} />
+        <CodeEditor
+          value={value}
+          onChange={handleChangeValue}
+          remainingTime={remainingTime}
+        />
         <Preview value={value} />
       </Flex>
     </Container>
