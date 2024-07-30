148ce546c5808ebaa758a3615cf0162b72fa15c7
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-07-30 14:04:44 +0700

    fix mantine notification mistake

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -8,7 +8,7 @@ import Head from "next/head";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ApplicationShell } from "@/components/core/appshell";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Notification } from "@mantine/core";
+import { Notifications } from "@mantine/notifications"
 
 const theme = createTheme({
   colors: {
@@ -41,7 +41,7 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <MantineProvider theme={theme} defaultColorScheme="dark">
-        <Notification />
+        <Notifications />
         <QueryClientProvider client={queryClient}>
           <ClerkProvider
             appearance={{
