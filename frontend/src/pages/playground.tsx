7bbdb87bcbdd738f68d760f0469be20409f494f9
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-08-01 13:10:35 +0700

    add tailwind cdn as default

--- frontend/src/pages/playground.tsx
+++ frontend/src/pages/playground.tsx
@@ -31,7 +31,7 @@ export default function Playground() {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
+     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         // Add your custom styles here
     </style>
