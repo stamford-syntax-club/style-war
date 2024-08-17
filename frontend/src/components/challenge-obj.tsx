e08703c0138d4bdd1048ac8144dd1aaf428be2c2
Pantorn <longpantorn@gmail.com>
2024-08-17 15:02:10 +0700

    challenge page add checkbox

--- frontend/src/components/challenge-obj.tsx
+++ frontend/src/components/challenge-obj.tsx
@@ -1,4 +1,4 @@
-import { Box, Title, Paper, List } from "@mantine/core";
+import { Box, Title, Paper, List, Checkbox } from "@mantine/core";
 import React from "react";
 
 type value = { value: (string | null)[] };
@@ -11,25 +11,13 @@ export default function Obj({ value }: value) {
       <Paper className="border border-gray-600 rounded p-1 h-[80vh] w-[50vw]">
         {
           value?.map((m) => {
-            if (m === null) {
-              return <List.Item key={`null`}>No objectives</List.Item>;
-            }
 
-            const M = m[0];
-
-            if (M >= '0' && M <= '9') {
-              return (
-                // <List type="ordered" size="sm" className="list-decimal">
-                // <List.Item key={`${m}`}>{m}</List.Item>
-                // </List>
-                <div>{m}</div>
-              )
-            } else {
-              return (
-                <List type="ordered" size="sm" className="list-disc m-3">
-                  <List.Item key={`${m}`}>{m}</List.Item>
-                </List>)
-            }
+            return (
+              <Checkbox
+                key={`${m}`}
+                label={m}
+                className="m-5"
+              />)
 
           })
         }
