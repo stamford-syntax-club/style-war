5a21a243d0c21d2156985e5dccee70fafb9e0f9e
chinathaip <cartoonabe@gmail.com>
2024-08-28 00:21:31 +0700

    frontend: accept remote image from syntax file server

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
-    domains: ['scrimba.com'],
+    domains: ["images.unsplash.com"],
+    domains: ["scrimba.com"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'ite442.stamford.dev',
-        port: '',
-        pathname: '/**/*',
+        protocol: "https",
+        hostname: "files.stamford.dev",
+        port: "",
+        pathname: "/**/*",
       },
-
     ],
   },
   output: "standalone",
