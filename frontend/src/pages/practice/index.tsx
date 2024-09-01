727d9cec668d351ad5a81d536aeadd4e725914a1
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2024-09-01 13:23:01 +0700

    practice finalize

--- frontend/src/pages/practice/index.tsx
+++ frontend/src/pages/practice/index.tsx
@@ -1,59 +1,90 @@
-import { Container, Flex, Modal, Text, Title } from "@mantine/core";
-import CodeEditor from "./pratice-editor";
-import { useState } from "react";
-import { useDisclosure } from "@mantine/hooks";
+import { Flex, Title, Select } from "@mantine/core";
+import PracticeEditor from "./pratice-editor";
 import Preview from "@/components/preview";
+import { useEffect, useState } from "react";
 import Challenge from "../challenge";
 
-export default function PracticePlayground() {
-  const [opened, { open, close }] = useDisclosure(true);
-  const [value, setValue] = useState(
-    `<!doctype html>
+export default function Playground() {
+  const [cssType, setCssType] = useState<string>("normal");
+
+  const cssOptions = (cssType: string) => {
+    let cdn = "";
+
+    switch (cssType) {
+      case "tailwind":
+        cdn = `<script src="https://cdn.tailwindcss.com"></script>`;
+        break;
+      case "bootstrap":
+        cdn = `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">`;
+        break;
+      default:
+        break;
+    }
+
+    return `<!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    ${cdn}
     <style>
-        // Add your custom styles here
+        /* Add your custom styles here */
     </style>
   </head>
   <body>
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
-    <!-- DO NOT REMOVE this line -->
-    <h1> Edit your code here </h1>
-    <p> Uncomment the script tags to use bootstrap & tailwind</p>
+    <h1>Edit your code here</h1>
+    <p>Choose the type of css you want to use from the dropdown above</p>
+    <p>Have Fun Practicing 🚀</p>
   </body>
-</html>`
-  );
+</html>`;
+  };
+
+  const [value, setValue] = useState(cssOptions(cssType));
+
+  useEffect(() => {
+    setValue(cssOptions(cssType));
+  }, [cssType]);
 
   const handleChangeValue = (newValue: string | undefined) => {
-    if (!newValue) {
-      return;
-    }
+    if (!newValue) return;
     setValue(newValue);
   };
 
   return (
-    <Container fluid>
-      <Modal
-        title="Practice Playground"
-        opened={opened}
-        onClose={close}
-        centered
-      >
-        <Text>
-          This is a practice playground where you get familiar with the platform
-          before the competition.
-        </Text>
-      </Modal>
-      {/* TODO: add hardcoded challenge here! */}
-      <Challenge objectives={[""]} isActive={true} imageUrl={""} />
-      <Flex justify="center" gap="md" align="center" mt="md">
-        <CodeEditor value={value} onChange={handleChangeValue} />
+    <Flex direction="column" justify="center" align="center">
+      <Flex justify="center" gap="md" align="center" w="80%">
+        <Select
+          label="Choose CSS type"
+          className="justify-self-start"
+          value={cssType}
+          onChange={(value) => setCssType(value || "normal")}
+          data={[
+            { value: "normal", label: "Normal CSS" },
+            { value: "tailwind", label: "Tailwind CSS" },
+            { value: "bootstrap", label: "Bootstrap CSS" },
+          ]}
+          mb="md"
+        />
+        {/* TODO: add hardcoded challenge here! */}
+        <Challenge
+          objectives={[
+            "Add a linear background from black (bottom left) to gray color (top right)",
+            "Create 0 circles of size 250px on top left and bottom right of the screen",
+            "Create a square in the middle of the screen",
+            "The square should be partially transparent and overlap with the 0 circles as shown in the reference",
+            "Add the word 'Style Wars' inside the square",
+          ]}
+          isActive={true}
+          imageUrl={
+            "https://files.stamford.dev/stamford-center/stylewars/practice-challenge.png"
+          }
+        />
+      </Flex>
+
+      <Flex justify="center" gap="md" align="center">
+        <PracticeEditor value={value} onChange={handleChangeValue} />
         <Preview value={value} />
       </Flex>
-    </Container>
+    </Flex>
   );
 }
